a347238df87645f53c9a4174f45d97458222e120
mingdixie <xiemingdi@gmail.com>
2021-08-27 13:17:52 +0000

    1

--- src/app/trade/trade.component.ts
+++ src/app/trade/trade.component.ts
@@ -18,17 +18,27 @@ export class TradeComponent implements OnInit {
   }
 
   clickBuyStock(){
-      this.typicodeService.buyStock(this.para).subscribe(
-        data=> this.buyMsg = 'trade completed!', 
-        err => this.buyMsg = 'trade failed! (Might due to symbol code not found)'
-      )
+    if(this.para.volume==0){
+      this.buyMsg = 'Volume can\'t be 0!'
+    }
+    else{
+        this.typicodeService.buyStock(this.para).subscribe(
+          data=> this.buyMsg = 'trade completed!', 
+          err => this.buyMsg = 'trade failed! (Might due to symbol code not found)'
+        )
+      }
     }
   
   clickSellStock(){
-      this.typicodeService.sellStock(this.paraSell).subscribe(
-        data=> this.sellMsg = 'trade completed!', 
-        err => this.sellMsg = 'trade failed! (Might due to volume exceed available amount or symbol code not found)'
-      )
+    if(this.paraSell.volume==0){
+      this.sellMsg = 'Volume can\'t be 0!'
+    }
+    else{
+        this.typicodeService.sellStock(this.paraSell).subscribe(
+          data=> this.sellMsg = 'trade completed!', 
+          err => this.sellMsg = 'trade failed! (Might due to volume exceed available amount or symbol code not found)'
+        )
+      }
     }
 
 }
